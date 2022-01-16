cf73bfcea71c6c0f4588a6ae290b1c1ee8db80c4
zearkiatos <caprilespe@outlook.com>
2022-01-16 17:34:49 -0300

    Optimization webpack for react and gitlab configuration

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,9 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin").default;
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin').default;
+
 module.exports = {
   entry: "./src/index.js",
   output: {
@@ -11,13 +9,9 @@ module.exports = {
     filename: "bundle.js"
   },
   resolve: {
-    extensions: [".js", ".jsx"],
-    aliast: {
-      "@components": path.resolve(__dirname, "src/components/"),
-      "@styles": path.resolve(__dirname, "src/styles/")
-    }
+    extensions: [".js", ".jsx"]
   },
-  mode: 'production',
+  mode: 'development',
   module: {
     rules: [
       {
@@ -46,11 +40,13 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: "[name].css"
-    }),
-    new CleanWebpackPlugin()
+    })
   ],
-  optimization: {
-    minimize: true,
-    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()]
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist")
+    },
+    compress: true,
+    port: 3000
   }
 };
