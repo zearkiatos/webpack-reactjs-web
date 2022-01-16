d5aba6165955e8b119e4f88852456a083adaef5d
zearkiatos <caprilespe@outlook.com>
2022-01-16 11:50:32 -0300

    Configuration plugins and loaders for React and dockerization project

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.js",
@@ -12,14 +13,26 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(.js|jsx)$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.html$/,
+        use: {
+            loader: "html-loader"
+        }
       }
     ]
   },
+  plugins: [
+      new HtmlWebpackPlugin({
+          template: './public/index.html',
+          filename: './index.html'
+      })
+  ],
   devServer: {
     static: {
       directory: path.join(__dirname, "dist")
